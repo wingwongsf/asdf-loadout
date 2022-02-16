9309c9ff2e7ec3cc2dce6f94c7e9ea6b6356bf3b
Wing Wong <wwong@neko360.com>
2022-02-16 15:26:39 -0800

    Test run completed and works. Added fallback listing.

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 Want to just start off "asdf" with all plugins available? Yep.
 
+  ./asdf_load_up.sh
 
+If the asdf plugin repo is successfully downloaded, you will be given a plugin
+count and asked to confirm the loadup by entering "GO AHEAD".
+
+If the asdf plugin repo cannot be downloaded, the script will fallback on a cached
+list of the plugins and also get confirmation.
+
+If you GO AHEAD, then all of the plugins will be added to asdf and available for the install commands.
+
+# Why?
+
+The plugins do not seem to be something that can be queried from the asdf tool itself. Since there
+is a plugin repo being maintained, this is a quick way to load up on the plugin url configs.
--- asdf_load_up.sh
+++ asdf_load_up.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env bash
 
-MISSING=0
-ASDF_REPO_PATH=
-for required_progs in asdf sed awk git 
-do
-  if [ `which ${required_progs} | grep -ci ${required_progs}` -lt 1 ]; then
-    echo ">> Please install '${required_progs}' or make sure it is in the PATH"
-    MISSING=1
-  fi
-done
-
-if [ $MISSING -gt 0 ]; then
-  echo "Usage: $0 <path to plugin repo>"
-  exit
-fi
+ASDF_REPO_PATH=./asdf-plugin-repo
 
 if [ ! -f ${ASDF_REPO_PATH}/README.md ]; then
-  echo "The asdf plugin repo is not checked out. checking out.
+  echo "The asdf plugin repo is not checked out. checking out."
+  git submodule update --init --remote --force --recursive
+  if [ ! -f ${ASDF_REPO_PATH}/README.md ]; then
+    echo ">> Error... couldn't get a working checkout of the asdf plugin repo."
+    fallback_count=`wc -l fallback/load_plugins`
+    echo "If you want to continue with the cached ${fallback_count} entries enter \"GO AHEAD\":"
+    read confirmation
+    if [ `echo "${confirmation}" | grep -i ahead | grep -ci go` -gt 0 ]; then
+      echo "Proceeding with massive plugin load up"
+      bash fallback/load_plugins
+      exit 0
+    fi
+  fi
+fi
+
+plugin_count=`find ${ASDF_REPO_PATH}/plugins -type f | wc -l`
+
+echo "Repo successfully downloaded. There are ${plugin_count} plugins to load. Enter \"GO AHEAD\" to add:"
+read confirmation
+if [ `echo "${confirmation}" | grep -i ahead | grep -ci go` -gt 0 ]; then
+  echo "Proceeding with massive plugin load up"
+
+  for plugin in `find ${ASDF_REPO_PATH}/plugins -type f`
+  do
+    #./asdf-plugin-repo/plugins/conform
+    plugin_name=`echo ${plugin} |  tr '/' '\012' | tail -1 | awk '{print $1}'`
+    plugin_url=`cat ${plugin} | awk '{print $3}' | sed -e 's#git/$#git#' -e 's#git/ #git#'`
+    asdf plugin add ${plugin_name} ${plugin_url}
+  done
 fi
